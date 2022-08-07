d52f8e32dbf22e0e0b9b3381ccb3f3d3feb6d0f4
odinhufnagl <odin.hufnagl@gmail.com>
2022-08-07 19:05:03 +0200

    test

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const createScheduler = () => {
   rule.minute = 0;
   rule.second = 0;
   rule.tz = FIRST_TIME_ZONE;
-  schedule.scheduleJob("*/4 * * * * *", createCronjobs);
+  schedule.scheduleJob(rule, createCronjobs);
 };
 
 createScheduler();
